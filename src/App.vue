d74a1b579660abca9ef46f4758d64df776bad734
Jim deVos <jtdevos@asu.edu>
2019-10-05 11:40:42 -0700

    checkpoint

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,63 @@
 <template>
-    <div id="app">
-        <HelloWorld msg="CheckboxList Demo" />
-        <form>
-            <fieldset>
-                <legend>Checkbox List</legend>
-                <CheckboxList :choices="myoptions" v-model="selectedItems" :size="10" />
-            </fieldset>
+  <div id="app">
+    <HelloWorld msg="VueDAR Component Demo" />
+    <form>
+      <fieldset class="demo-options">
+        <legend>Demo Options</legend>
+        <div class="form-control">
+          <label for="selDatasource">Data Source</label>
+          <select id="selDatasource" disabled>
+            <option>Not Yet Available</option>
+            <option>Not Yet Available</option>
+            <option>Not Yet Available</option>
+          </select>
+        </div>
 
-            <fieldset>
-                <legend>Select List</legend>
-                <SelectList name="myselectlist" :options="myoptions" />
-            </fieldset>
+        <div class="form-control">
+          <label>Viewable Controls</label>
+          <label>
+            <input type="checkbox" v-model="bShowSelectList" />SelectList
+          </label>
+          <label>
+            <input type="checkbox" v-model="bShowCheckboxList" />CheckboxList
+          </label>
+        </div>
 
-            <fieldset>
-                <legend>Selected Values</legend>
-                <div class="selected">
-                    <code v-for="item in selectedItems" v-bind:key="item">{{item}}</code>
-                </div>
-            </fieldset>
-        </form>
-    </div>
+        <div class="form-control">
+          <label>Actions</label>
+          <br />
+          <button type="button" class="btn" @click="selectAll">Select All</button>
+          <button type="button" class="btn" @click="selectRandom">Select Random</button>
+          <button type="button" class="btn" @click="selectNone">Select None</button> •
+          <button type="button" class="btn" @click="serialize">Serialize</button>
+          <button type="button" class="btn" @click="deserialize">Deserialize</button>
+        </div>
+      </fieldset>
+
+      <fieldset>
+        <legend>
+          Select List
+          <span v-if="!bShowSelectList">(hidden)</span>
+        </legend>
+        <SelectList v-if="bShowSelectList" name="myselectlist" :options="myoptions" :size="10" />
+      </fieldset>
+
+      <fieldset>
+        <legend>
+          Checkbox List
+          <span v-if="!bShowCheckboxList">(hidden)</span>
+        </legend>
+        <CheckboxList v-if="bShowCheckboxList" :choices="myoptions" v-model="selectedItems" />
+      </fieldset>
+
+      <fieldset>
+        <legend>Selected Values</legend>
+        <div class="selected">
+          <code v-for="item in selectedItems" v-bind:key="item">{{item}}</code>
+        </div>
+      </fieldset>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -28,88 +66,125 @@ import HelloWorld from "./components/HelloWorld";
 import SelectList from "./components/SelectList";
 import Mimdata from "./assets/mimdata.js";
 
+var _random = Math.random.bind(Math);
+
 function getdata() {
-    var mimdata = Mimdata;
-    var datasetReferences = mimdata.datasetReferences[0].columns
-    var subdata = datasetReferences;
-    var clone = deepclone(subdata);
-    return clone;
+  var mimdata = Mimdata;
+  var datasetReferences = mimdata.datasetReferences[0].columns;
+  var subdata = datasetReferences;
+  var clone = deepclone(subdata);
+  return clone;
 }
 
 function deepclone(obj) {
-    return JSON.parse(JSON.stringify(obj));
+  return JSON.parse(JSON.stringify(obj));
 }
 
-function generateOptions(columnName){
-    var columns = getdata().reduce( (obj, column) => {
-        obj[column.name] = column;
-        return obj;
-    }, {});
-    var columnValues = columns[columnName].values;
-    var opts = columnValues.map( v => ({label: v, value: v}));
-    return opts
+function generateOptions(columnName) {
+  var columns = getdata().reduce((obj, column) => {
+    obj[column.name] = column;
+    return obj;
+  }, {});
+  var columnValues = columns[columnName].values;
+  var opts = columnValues.map(v => ({ label: v, value: v }));
+  return opts;
 }
 
 export default {
-    name: "App",
-    components: { HelloWorld, CheckboxList, SelectList },
-      data: function() {
-        return {
-          lovingVue: false,
-          selectedItems: [],
-          myoptions: generateOptions("figurative_specific")
-        };
-      },
+  name: "App",
+  components: { HelloWorld, CheckboxList, SelectList },
+  data: function() {
+    return {
+      lovingVue: false,
+      selectedItems: [],
+      myoptions: generateOptions("figurative_specific"),
 
-    methods: {
-        doupdate: function() {
-            console.log("doupdate");
-        },
+      /// Demo Options
+      bShowCheckboxList: false,
+      bShowSelectList: true
+    };
+  },
 
-        sendOuterUpdate: function() {
-            this.selectedItems = ["1", "3"];
-            this.$set(this.selectedItems, 0, "1");
-        }
+  methods: {
+    doupdate: function() {
+      console.log("doupdate");
     },
-    watch: {
-        listupdate: function() {
-            console.log("listupdate event");
-        }
+
+    sendOuterUpdate: function() {
+      this.selectedItems = ["1", "3"];
+      this.$set(this.selectedItems, 0, "1");
+    },
+
+    clear: function() {
+      this.bShowCheckboxList = true;
+      this.bShowSelectList = true;
+    },
+
+    selectAll: function() {
+
+    },
+
+    selectRandom: function() {
+      
+    },
+
+    selectNone: function() {
+    },
+
+    serialize: function() {
+    },
+
+    deserialize: function() {
+    },
+
+
+  },
+  watch: {
+    listupdate: function() {
+      console.log("listupdate event");
     }
+  }
 };
 </script>
 
 <style>
 #app {
-    font-family: "Avenir", Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-    margin: 10pt;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  margin: 10pt;
 }
 
 div.checkboxlist {
-    text-align: left;
+  text-align: left;
 }
 
 fieldset {
-    margin: 10pt 0;
+  margin: 10pt 0;
+  text-align: left;
+  border-radius: 6pt;
+  border: 2pt solid #ccc;
 }
 code {
-    display: inline-block; 
-    font-size: 14pt;
-    color: #bbb;
-    text-align: left;
-    margin: 1em 0.5em;
-    padding: 3px 4px;
-    border: 1px solid #888;
-    border-radius: 6px;
-    line-height: normal;
+  display: inline-block;
+  font-size: 14pt;
+  color: #bbb;
+  text-align: left;
+  margin: 1em 0.5em;
+  padding: 3px 4px;
+  border: 1px solid #888;
+  border-radius: 6px;
+  line-height: normal;
 }
 
 label {
-    font-size: 14pt;
+  font-size: 14pt;
+}
+
+select[multiple] {
+  border-radius: 5pt;
 }
 </style>
--- src/components/SelectList.vue
+++ src/components/SelectList.vue
@@ -1,63 +1,66 @@
 <template>
-<div class="selectlist">
-    <h2> Select List goes here</h2>
-    <select :name="name" :multiple="size > 1" :size="size" class="">
-        <option  
-            v-for="(option, index) in options" 
-            :key="name + 'option_' + index"
-            :value="option.value"
-            
-            >Option {{option.label}}</option>
+  <div class="selectlist">
+    <select :name="name" :multiple="size > 1" :size="size" class>
+      <option
+        v-for="(option, index) in options"
+        :key="name + 'option_' + index"
+        :value="option.value"
+      >{{option.label}}</option>
     </select>
-</div>
-    
+  </div>
 </template>
 
 <script>
 export default {
-    name:"SelectList",
-    props: {
-        "name": {
-            type: String,
-            required: true
-        },
-        "options": {
-            type: [Object, Array],
-            required: true
-        },
-        "selectedOptions": {
-            required: false, 
-            type: Array,
-            default: function() {
-                return []
-            }
-        },
-        "labelKey": {
-            type: String,
-            required: false,
-            default: "label"
-        },
-        "valueKey": {
-            type: String,
-            required: false,
-            default: "value"
-        },
-        "size": {
-            type: Number,
-            required: false, 
-            default: 1
-        }
+  name: "SelectList",
+  props: {
+    name: {
+      type: String,
+      required: true
     },
+    options: {
+      type: [Object, Array],
+      required: true
+    },
+    selectedOptions: {
+      required: false,
+      type: Array,
+      default: function() {
+        return [];
+      }
+    },
+    labelKey: {
+      type: String,
+      required: false,
+      default: "label"
+    },
+    valueKey: {
+      type: String,
+      required: false,
+      default: "value"
+    },
+    size: {
+      type: Number,
+      required: false,
+      default: 1
+    }
+  },
 
-    mounted: function() {},
-    computed: {},
-    methods: {},
-
-}
+  mounted: function() {},
+  computed: {},
+  methods: {}
+};
 </script>
 
 
 
 <style scoped>
+div.selectlist, option {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+}
 
+label,
+option {
+  font-size: 14pt;
+}
 </style>
